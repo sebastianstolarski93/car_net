64eac4ed65de6ef7d005d214443be523b629d4a5
Sebastian Stolarski <sebastian.stolarski@ftsgps.com>
2018-01-01 23:32:52 +0100

    add store method

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    public function offer() {
+        return $this->belongsToMany(Offer::class);
+    }
 }
--- app/Offer.php
+++ app/Offer.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Offer extends Model
 {
-    //
+    public function user() {
+        return $this->belongsToMany(User::class);
+    }
+
+    protected $fillable = [
+        'title', 'price', 'description', 'photos', 'equipment', 'specification', 'start_date', 'end_date', 'user_id', 'category_id'
+    ];
 }
